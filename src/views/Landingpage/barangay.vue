4ec9a790494c230ac1ec8af1dba07e17b12ca065
Ramirez, Jennifer M <144778693+jen2112@users.noreply.github.com>
2023-12-02 17:21:36 +0800

    frontend

--- src/views/Landingpage/barangay.vue
+++ src/views/Landingpage/barangay.vue
@@ -5,8 +5,8 @@
 				<div class="section-header">
 					<h2>Barangay</h2>
 					<p>Calapan has 62 barangays as shown in the following table.</p>
-					<p>Testing vue js in github .</p>
 				</div>
+				<demographys/>
             </div>
     </section>
 	<footers/>
@@ -15,10 +15,12 @@
 
 <script>
 import headers from '@/views/Landingpage/include/header.vue'
+import demographys from '@/views/Landingpage/include/demography.vue'
 import footers from '@/views/Landingpage/include/footer.vue'
 export default{
 	components:{
 		headers,
+		demographys,
 		footers
 	}
 }
